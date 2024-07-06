796aaa36454e0d924816bd2f9a55a9808366f746
HJIWE <121221059+HJIWE@users.noreply.github.com>
2024-07-06 23:53:12 +0800

    Add files via upload

---
+++ pages/home/home.vue
@@ -0,0 +1,62 @@
+<template>
+	<view class="container">
+		<view class="top_box">
+			<view class="top_back" :style="{backgroundColor:boxColor}">
+
+			</view>
+			<view class="banner">
+				<uni-swiper-dot class="uni-swiper-dot-box" :info="info" :current="current" mode="dot">
+					<swiper class="swiper-box" @change="swiperChange">
+						<swiper-item v-for="(item, index) in info" :key="index">
+							<image :src="item" mode="aspectFit"></image>
+						</swiper-item>
+					</swiper>
+				</uni-swiper-dot>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				boxColor:"rgb(255,85,127)",
+				boxColorLs:["rgb(255,85,127)","rgb(255,170,0)",
+				"rgb(170,0,0)","rgb(0,170,127)"],
+				info: ["./1.jpg", "./2.jpg", "./3.jpg", "./4.jpg"],
+				current:0,
+			}
+		},
+		methods: {
+			swiperChange(e){
+				this.current = e.detail.current;
+				this.boxColor = this.boxColorLs[this.current];
+			}
+		}
+	}
+</script>
+
+<style lang="scss">
+	.container {
+		font-size: 14px;
+		line-height: 24px;
+	}
+
+	.top_box {
+		.top_back {
+			height: 400upx;
+			width: 100%;
+			background-color: $uni-primary;
+		}
+
+		.banner {
+			text-align: center;
+			margin-top: -300upx;
+			image {
+				width: 100%;
+				height: 100%;
+			}
+		}
+	}
+</style>
---
+++ pages/my/my-camera.vue
@@ -0,0 +1,55 @@
+
+<template>
+    <view>
+        <camera device-position="back" flash="off" @error="error" style="width: 100%; height: 720px;"></camera>
+        <button type="primary" @click="submitCamera">拍照上传</button>
+    </view>
+</template>
+
+<script>
+	export default {
+	    data() {
+	        return {
+	            src: ""
+	        }
+	    },
+	    methods: {
+	         /*takePhoto() {
+	                 const ctx = uni.createCameraContext();
+	                 ctx.takePhoto({
+	                     quality: 'high',
+	                     success: (res) => {
+	                         this.src = res.tempImagePath;
+	                         console.log('Photo taken, path:', this.src); // 调试输出
+	                         
+                             // 延迟一秒导航以确保路径获取成功
+                             setTimeout(() => {
+                                uni.navigateTo({
+                                    url: "/pages/my/my-argue?src=" + encodeURIComponent(this.src)
+                                });
+                             }, 1000);
+	                     }
+	                 });
+	             },*/
+			submitCamera() {
+				const ctx = uni.createCameraContext();
+				ctx.takePhoto({
+				    quality: 'high',
+				    success: (res) => {
+				        this.src = res.tempImagePath;
+			        uni.navigateBack({
+			            delta: 1,
+			            success: () => {
+			                const eventChannel = this.getOpenerEventChannel();
+			                eventChannel.emit('acceptComment', {src:this.src });
+			            }
+			        });
+				}
+			});
+			},
+	        error(e) {
+	            console.log(e.detail);
+	        }
+	    }
+	}
+</script>
