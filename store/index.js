52a6be8df51679ede0a2e3963eb42ed337fcb2d7
HeadAgainst <139834464+HeadAgainst@users.noreply.github.com>
2024-07-07 13:10:10 +0800

    Add files via upload

--- store/index.js
+++ store/index.js
@@ -4,10 +4,57 @@ const store = createStore({
 	state:{//存放状态
 		"token":"",
 		"username":"foo",
-		
+		"cart":[{
+			id:"1",
+			num:1,
+			price:10
+		}],
+		mapGoodNum:{
+			"1":{
+				num:1
+			}
+		},
+		sortCurrent:0
 	},
 	mutations:{
-		
+		addOneGood(state, payload){
+			const {id, num, price} = payload;
+			let i = 0;
+			for(i = 0; i < this.state.cart.length; i++){
+				if(id == this.state.cart[i].id){
+					this.state.cart[i].num += 1;
+					this.state.mapGoodNum[id].num += 1;
+					break;
+				}
+			}
+			if(i >= this.state.cart.length){
+				this.state.cart.push({
+					id:id,
+					num:num,
+					price:price
+				})
+				this.state.mapGoodNum[id] = {};
+				this.state.mapGoodNum[id].num = 1;
+			}
+		},
+		subtractOneGood(state, payload){
+			const {id, num} = payload;
+			let i = 0;
+			for(i = 0; i < this.state.cart.length; i++){
+				if(id == this.state.cart[i].id){
+					this.state.cart[i].num -= 1;
+					this.state.mapGoodNum[id].num -= 1;
+					if(this.state.cart[i].num == 0){
+						this.state.cart.splice(i, 1);
+						delete this.state.mapGoodNum[id];
+					}
+					break;
+				}
+			}
+		},
+		changSortCurrent(state, current){
+			this.state.sortCurrent = current;
+		}
 	},
 	getters:{
 		
