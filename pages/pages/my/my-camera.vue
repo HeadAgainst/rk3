61e8a1aa92bbe87eb64dea8b87aa3431ba6bc992
HJIWE <121221059+HJIWE@users.noreply.github.com>
2024-07-07 13:06:09 +0800

    Add files via upload

---
+++ pages/pages/my/my-camera.vue
@@ -0,0 +1,50 @@
+
+<template>
+    <view>
+        <camera device-position="back" flash="off" @error="error" style="width: 100%; height: 660px;"></camera>
+        <button type="primary" @click="takePhoto">拍照</button>
+		<button type="warn" @click="submitCamera">上传</button>
+    </view>
+</template>
+
+<script>
+	export default {
+	    data() {
+	        return {
+	            src: ""
+	        }
+	    },
+	    methods: {
+	         takePhoto() {
+	                 const ctx = uni.createCameraContext();
+	                 ctx.takePhoto({
+	                     quality: 'high',
+	                     success: (res) => {
+	                         this.src = res.tempImagePath;
+	                          // 调试输出
+	                         
+                             // 延迟一秒导航以确保路径获取成功
+                             setTimeout(() => {
+                                /*uni.navigateTo({
+                                    url: "/pages/my/my-argue?src=" + encodeURIComponent(this.src)
+                                });*/
+                             }, 1000);
+	                     }
+	                 });
+	             },
+			submitCamera() {
+			        uni.navigateBack({
+			            delta: 1,
+			            success: () => {
+			                const eventChannel = this.getOpenerEventChannel();
+			                eventChannel.emit('acceptCamera', { src:this.src });
+							console.log('Photo taken, path:', this.src);
+			            }
+			        });
+			},
+	        error(e) {
+	            console.log(e.detail);
+	        }
+	    }
+	}
+</script>
